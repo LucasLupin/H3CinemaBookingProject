3a7925ab8dae693c6703b4cae1a3f92d95dcd08d
LZ <lucasziegler@live.dk>
2024-05-05 18:01:20 +0200

    New Changes

--- CinemaBookingWeb/src/app/app.component.ts
+++ CinemaBookingWeb/src/app/app.component.ts
@@ -14,22 +14,6 @@ export class AppComponent implements OnInit {
     constructor(private authService: AuthService, private router: Router, private storageService : LocalStorageGeneric) {}
 
     ngOnInit() {
-      this.storageService.handleLocalStorage();
-      console.log('App component initialized');
-      // this.authService.currentUser.subscribe(user => {
-      //   this.isLoggedIn = user != null;
-      // });
-      // this.router.navigate(['']);
-      // if (!this.isLoggedIn) {
-      //   this.authService.login('guest', 'password').subscribe(
-      //     () => {
-      //       // Redirect to frontpage
-      //       this.router.navigate(['']);
-      //     },
-      //     (error) => {
-      //       console.log('Login failed:', error);
-      //     }
-      //   );
-      // }
+      console.log("App component initialized")
     }
   }
